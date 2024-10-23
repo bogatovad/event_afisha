2b871f21d12f00cff25804eac0fcb43314d96ba9
shmatozz <baryshev.matvey@yandex.ru>
2024-10-23 20:14:33 +0300

    feat: add info and feedback pages

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout() {
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="feedback" options={{ headerShown: false }} />
         <Stack.Screen name="events" options={{ headerShown: false }} />
+        <Stack.Screen name="about" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,11 +1,13 @@
 import Box from "@/components/Box";
+import Text from "@/components/Text";
 import {useEffect} from "react";
 import {useTagsStore} from "@/stores/useTagsStore";
 import LoadingCard from "@/components/cards/LoadingCard";
 import ErrorCard from "@/components/cards/ErrorCard";
 import TagCard from "@/components/cards/TagCard";
-import {Animated, FlatList} from "react-native";
+import {Animated, FlatList, Pressable} from "react-native";
 import ScrollView = Animated.ScrollView;
+import {router} from "expo-router";
 
 export default function HomeScreen() {
   const { tags, isLoading, hasError, fetchTags } = useTagsStore();
@@ -35,6 +37,33 @@ export default function HomeScreen() {
         padding="l"
         gap="m"
       >
+        <Pressable
+          onPress={ () => router.push("/about") }
+          style={{
+            width: "100%"
+          }}
+        >
+          <Box
+            width={"100%"}
+            height={40}
+            flexDirection="row"
+            justifyContent="center"
+            alignItems="center"
+            padding={"m"}
+            backgroundColor="button_color"
+            style={{
+              borderRadius: 12,
+            }}
+          >
+            <Text
+              variant="body"
+              color="button_text_color"
+            >
+              { "О нас" }
+            </Text>
+          </Box>
+        </Pressable>
+
         <FlatList
           data={tags}
           renderItem={({ item }) =>
