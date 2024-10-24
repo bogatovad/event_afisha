0185a8c29c54552d4d5a1de223797e423b7a00f4
shmatozz <baryshev.matvey@yandex.ru>
2024-10-24 15:04:37 +0300

    fix: remove timeout

--- frontend/services/AxiosConfig.ts
+++ frontend/services/AxiosConfig.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'https://afishabot.ru/api/v1',
-  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
