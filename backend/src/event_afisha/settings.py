e7861055226e6a70268867eaff09e3e852035611
Artem Bogatov <artembbogatov@yandex.ru>
2024-10-24 12:38:47 +0300

    feat: add https api

--- backend/src/event_afisha/settings.py
+++ backend/src/event_afisha/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-u*q+ol&)tp5r6#qrw)y28e_c0vmv81jq*5oto-3s9*dc%c&uk=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "130.193.41.98"]
+ALLOWED_HOSTS = ["0.0.0.0", "130.193.41.98", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -124,7 +124,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+MEDIA_URL = ""
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
