0a9e2fcc306271ebd5600f05906a86d124db57a2
Matvey Baryshev <baryshev.matvey@yandex.ru>
2024-12-04 13:36:55 +0300

    fix: another try to fix iOS layout

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -4,62 +4,70 @@ import {Tabs} from "expo-router";
 import {Theme} from "@/shared/providers/Theme";
 import Icon from "@/shared/ui/Icons/Icon";
 import {useSafeAreaInsets} from "@/shared/providers/SafeAreaWrapper";
+import {SafeAreaView} from "react-native-safe-area-context";
 
 export default function TabLayout() {
   const theme = useTheme<Theme>();
   const insets = useSafeAreaInsets();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: theme.colors.button_color,
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarStyle: {
-          backgroundColor: theme.colors.bg_color,
-          borderTopWidth: 0,
-        },
-      }}
-      sceneContainerStyle={{
+    <SafeAreaView
+      style={{
+        flex: 1,
         backgroundColor: theme.colors.bg_color,
         paddingBottom: insets.bottom, paddingTop: insets.top,
         paddingLeft: insets.left, paddingRight: insets.right,
       }}
     >
-      <Tabs.Screen
-        name="feed"
-        options={{
-          tabBarIcon: ({color}) => <Icon name={"home"} size={30} color={color}/>,
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: theme.colors.button_color,
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: theme.colors.bg_color,
+            borderTopWidth: 0,
+          },
         }}
-      />
+        sceneContainerStyle={{
+          backgroundColor: theme.colors.bg_color,
+        }}
+      >
+        <Tabs.Screen
+          name="feed"
+          options={{
+            tabBarIcon: ({color}) => <Icon name={"home"} size={30} color={color}/>,
+          }}
+        />
 
-      <Tabs.Screen
-        name="tags"
-        options={{
-          tabBarIcon: ({color}) => <Icon name={"tags"} size={30} color={color}/>,
-        }}
-      />
+        <Tabs.Screen
+          name="tags"
+          options={{
+            tabBarIcon: ({color}) => <Icon name={"tags"} size={30} color={color}/>,
+          }}
+        />
 
-      <Tabs.Screen
-        name="calendar"
-        options={{
-          tabBarIcon: ({color}) => <Icon name={"calendar"} size={30} color={color}/>,
-        }}
-      />
+        <Tabs.Screen
+          name="calendar"
+          options={{
+            tabBarIcon: ({color}) => <Icon name={"calendar"} size={30} color={color}/>,
+          }}
+        />
 
-      <Tabs.Screen
-        name="likes"
-        options={{
-          tabBarIcon: ({color}) => <Icon name={"likeFilled"} size={28} color={color}/>,
-        }}
-      />
+        <Tabs.Screen
+          name="likes"
+          options={{
+            tabBarIcon: ({color}) => <Icon name={"likeFilled"} size={28} color={color}/>,
+          }}
+        />
 
-      <Tabs.Screen
-        name="profile"
-        options={{
-          tabBarIcon: ({color}) => <Icon name={"user"} size={30} color={color}/>,
-        }}
-      />
-    </Tabs>
+        <Tabs.Screen
+          name="profile"
+          options={{
+            tabBarIcon: ({color}) => <Icon name={"user"} size={30} color={color}/>,
+          }}
+        />
+      </Tabs>
+    </SafeAreaView>
   );
 }
