8c96010f48e97d3e498bb760e346166a4beb9b96
Matvey Baryshev <baryshev.matvey@yandex.ru>
2025-01-28 17:50:36 +0300

    feat: move EventsSwiper state out of component

--- frontend/pages/feed/ui/FeedPage.tsx
+++ frontend/pages/feed/ui/FeedPage.tsx
@@ -1,14 +1,40 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {EventsSwiper} from "@/widgets/events-swiper";
-import {Box} from "@/shared/ui";
+import {Box, ErrorCard, LoadingCard} from "@/shared/ui";
+import {useFeedStore} from "@/features/content";
+import {getPeriodBorders} from "@/shared/scripts/date";
+import {useConfig} from "@/shared/providers/TelegramConfig";
+import {useCalendarStore} from "@/features/dates";
 
 export const FeedPage = () => {
+  console.log(123);
+  const username = useConfig().initDataUnsafe.user.username;
+  const {
+    feed,
+    isLoading, hasError,
+    swipedAll, setSwipedAll,
+    fetchFeed,
+  } = useFeedStore();
+  const { selectedDays} = useCalendarStore();
+
+  useEffect(() => {
+    const borders = getPeriodBorders(Object.keys(selectedDays));
+    fetchFeed({
+      username: username,
+      date_start: borders.date_start,
+      date_end: borders.date_end
+    });
+  }, [selectedDays]);
+
+  if (isLoading) return <LoadingCard style={{ flex: 1, height: "100%", width: "100%" }}/>
+  if (hasError) return <ErrorCard />
+
   return (
     <Box
       flex={1}
       flexDirection="column"
     >
-      <EventsSwiper/>
+      <EventsSwiper events={feed} swipedAll={swipedAll} setSwipedAll={setSwipedAll}/>
     </Box>
   )
 }
