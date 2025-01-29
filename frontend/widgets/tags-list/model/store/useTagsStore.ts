1c861b8863c55852e27b1917a0e0b551155d7f28
Matvey Baryshev <baryshev.matvey@yandex.ru>
2025-01-29 12:47:17 +0300

    fix: update feed after update preferences

--- frontend/widgets/tags-list/model/store/useTagsStore.ts
+++ frontend/widgets/tags-list/model/store/useTagsStore.ts
@@ -2,6 +2,9 @@ import { create } from 'zustand';
 import {Tag} from "@/entities/tag";
 import tagsService from "@/widgets/tags-list/api/TagsService";
 import {PreferencesRequest, TagsRequest} from "@/widgets/tags-list";
+import {useFeedStore} from "@/features/content";
+import {getPeriodBorders} from "@/shared/scripts/date";
+import {useCalendarStore} from "@/features/dates";
 
 interface TagsState {
   tags: Tag[];
@@ -19,20 +22,33 @@ export const useTagsStore = create<TagsState>((set) => ({
   hasError: false,
 
   onTagLike: (params: PreferencesRequest) => {
+    const updateFeed = () => {
+      const borders = getPeriodBorders(Object.keys(useCalendarStore.getState().selectedDays));
+      useFeedStore.getState().fetchFeed({
+        username: params.username,
+        date_start: borders.date_start,
+        date_end: borders.date_end,
+      })
+    }
+
     set((state) => {
+      let newPreferences: number[];
+
       if (state.preferences.find((p) => p === params.tag_id)) {
         tagsService.deletePreference(params)
-          .then((response) => console.log(response.data))
+          .then((_response) => updateFeed())
           .catch((e) => console.log(e));
 
-        return { preferences: state.preferences.filter((p) => p !== params.tag_id) };
+        newPreferences = state.preferences.filter((p) => p !== params.tag_id);
+      } else {
+        tagsService.savePreference(params)
+          .then((_response) => updateFeed())
+          .catch((e) => console.log(e));
+
+        newPreferences = [...state.preferences, params.tag_id];
       }
 
-      tagsService.savePreference(params)
-        .then((response) => console.log(response.data))
-        .catch((e) => console.log(e));
-
-      return { preferences:[...state.preferences, params.tag_id] };
+      return { tags: sortTagsByPreferences(state.tags, newPreferences), preferences: newPreferences };
     });
   },
 
