6a36cffe4741030cbebd921d80dec34adce9c865
Artem Bogatov <artembbogatov@yandex.ru>
2025-02-21 18:28:16 +0300

    feat: add

--- backend/src/event/admin.py
+++ backend/src/event/admin.py
@@ -1,42 +1,46 @@
 from django.contrib import admin
-from event.models import Content, Tags, Like, User, Feedback, RemovedFavorite, MacroCategory, UserCategoryPreference
+from event.models import (
+    Content,
+    Tags,
+    Like,
+    User,
+    MacroCategory,
+)
 
 
 @admin.register(Content)
 class ContentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'image', 'contact', 'get_tags', 'date', 'time', 'cost', 'location', 'city')
+    list_display = (
+        "name",
+        "description",
+        "image",
+        "contact",
+        "date_start",
+        "date_end",
+        "time",
+        "location",
+        "cost",
+        "city",
+        "unique_id",
+        "get_tags",
+    )
 
 
 @admin.register(Tags)
 class TagsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'macro_category')
+    list_display = ("name", "description")
 
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
-    list_display = ('user', 'content', 'value', 'created')
+    pass
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'created')
-
-
-@admin.register(Feedback)
-class FeedbackAdmin(admin.ModelAdmin):
-    list_display = ('user', 'message', 'created')
-
-
-@admin.register(RemovedFavorite)
-class RemovedFavoriteAdmin(admin.ModelAdmin):
-    list_display = ('user', 'content', 'removed_at')
+    list_display = ("username",)
 
 
 @admin.register(MacroCategory)
 class MacroCategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
-
-
-@admin.register(UserCategoryPreference)
-class UserCategoryPreferenceAdmin(admin.ModelAdmin):
-    list_display = ('user', 'tag')
+    list_display = ("name",)
