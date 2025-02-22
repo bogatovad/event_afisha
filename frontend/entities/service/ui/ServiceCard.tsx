0a88a6e526a2c35ca929ad1495ba2a4d27daf100
Matvey Baryshev <baryshev.matvey@yandex.ru>
2025-02-22 12:53:05 +0300

    feat: new services layout

--- frontend/entities/service/ui/ServiceCard.tsx
+++ frontend/entities/service/ui/ServiceCard.tsx
@@ -2,45 +2,66 @@ import React from "react";
 import {Service} from "@/entities/service/model/types/services.types";
 import {Box, Text, WebLottieView} from "@/shared/ui";
 import Illustration from "@/shared/ui/Illustrations/Illustration";
+import {Pressable} from "react-native";
 
 interface ServiceCardProps {
   service: Service;
+  onPress: () => void;
 }
 
 export const ServiceCard: React.FC<ServiceCardProps> = ({
-  service
+  service,
+  onPress
 }) => {
+  let startX = 0;
+
   return (
-    <Box
-      flex={1}
-      alignItems={"center"} justifyContent={"center"}
-      borderWidth={3}
-      paddingHorizontal={"m"}
-      gap={"m"}
+    <Pressable
+      onPressIn={(event) => {
+        startX = event.nativeEvent.pageX;
+      }}
+      onPressOut={(event) => {
+        const endX = event.nativeEvent.pageX;
+        if (Math.abs(endX - startX) < 10) {
+          onPress();
+        }
+      }}
       style={{
-        backgroundColor: service.id == "organizers" || service.id == "trips" ? "#E1E1E1" : "#F8F8F8",
-        borderRadius: 30, borderColor: "#DBDFFB", paddingVertical: 100,
-        flex: 1,
-        shadowColor: '#4C4C4C',
-        shadowOffset: {
-          width: 10,
-          height: 10,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 15,
+        height: "100%"
       }}
     >
-      <Text variant={"serviceName"} textAlign={"center"} color={"black"} selectable={false}>
-        {service.name}
-      </Text>
+      <Box
+        flex={1}
+        alignItems={"center"} justifyContent={"center"}
+        borderWidth={3}
+        gap={"m"}
+        style={{
+          height: "100%",
+          backgroundColor: service.id == "organizers" || service.id == "trips" ? "#E1E1E1" : "#F8F8F8",
+          borderRadius: 30, borderColor: "#DBDFFB",
+          paddingVertical: 100,
+          paddingHorizontal: 32,
+          shadowColor: '#4C4C4C',
+          shadowOffset: {
+            width: 10,
+            height: 10,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 15,
+        }}
+      >
+        <Text variant={"serviceName"} textAlign={"center"} color={"black"} selectable={false}>
+          {service.name}
+        </Text>
 
-      {service.id == "events" && <WebLottieView src={require("@/shared/assets/lottie/events.json")}/>}
-      {service.id == "places" && <WebLottieView src={require("@/shared/assets/lottie/places.json")}/>}
-      {(service.id == "trips" || service.id == "organizers") && <Illustration name={service.illustration} width={"100%"} height={"100%"}/>}
+        {service.id == "events" && <Box width={"100%"}><WebLottieView src={require("@/shared/assets/lottie/events.json")}/></Box>}
+        {service.id == "places" && <Box width={"100%"}><WebLottieView src={require("@/shared/assets/lottie/places.json")}/></Box>}
+        {(service.id == "trips" || service.id == "organizers") && <Illustration name={service.illustration} width={"100%"} height={"100%"}/>}
 
-      <Text variant={"serviceDescription"} textAlign={"center"} color={"black"} selectable={false}>
-        {service.description}
-      </Text>
-    </Box>
+        <Text variant={"serviceDescription"} textAlign={"center"} color={"black"} selectable={false}>
+          {service.description}
+        </Text>
+      </Box>
+    </Pressable>
   )
 }
--- frontend/pages/tags/ui/ServicesPage.tsx
+++ frontend/pages/tags/ui/ServicesPage.tsx
@@ -1,74 +1,95 @@
 import React from "react";
 import Animated, {
   interpolateColor, useAnimatedStyle,
-  useSharedValue, withTiming
+  useSharedValue
 } from "react-native-reanimated";
-import Swiper from "react-native-deck-swiper";
 import {ServiceCard, Services, ServicesColors} from "@/entities/service";
-import {LinearGradient} from "expo-linear-gradient";
 import {useRouter} from "expo-router";
+import Carousel from "react-native-reanimated-carousel/src/components/Carousel";
+import {Dimensions} from "react-native";
+import {BlurView} from "expo-blur";
+import {Box} from "@/shared/ui";
+
+const window = Dimensions.get("window");
 
 export const ServicesPage = () => {
   const router = useRouter();
 
-  const cardIndex = useSharedValue(0);
   const swipeProgress = useSharedValue(0);
 
   const animatedBackgroundStyle = useAnimatedStyle(() => {
-    const currentIndex = Math.floor(cardIndex.value);
-    const nextIndex = (currentIndex + 1) % Object.keys(ServicesColors).length;
-
     const backgroundColor = interpolateColor(
       swipeProgress.value,
-      [0, 1],
-      [Object.values(ServicesColors)[currentIndex], Object.values(ServicesColors)[nextIndex]],
+      [0, 1, 2, 3, 4],
+      [...Object.values(ServicesColors), Object.values(ServicesColors)[0]],
       'RGB', { gamma: 2 }
     );
 
-    return { backgroundColor };
+    return { backgroundColor, opacity: 0.8 };
   });
 
   return (
-    <Animated.View
-      style={[{ flex: 1 }, animatedBackgroundStyle]}
-    >
-      <Swiper
-        cards={Services}
-        renderCard={(service) => <ServiceCard service={service}/>}
-        infinite
-        stackSize={4}
-        containerStyle={{ backgroundColor: "transparent", paddingTop: 30 }}
-        cardHorizontalMargin={30}
-        cardVerticalMargin={100}
-        stackSeparation={-20}
-        onTapCard={(index) => {
-          if (Services[index].id != "organizers" && Services[index].id != "trips") {
-            router.push({
-              pathname: '/tags/[service]',
-              params: { service: Services[index].id }
-            })
-          }
-        }}
-        onSwiping={(x, y) => {
-          swipeProgress.value = Math.min(Math.max(Math.abs(x), Math.abs(y)) / 100, 1);
-        }}
-        onSwiped={(index) => {
-          swipeProgress.value = withTiming(1, { duration: 150 }, () => {
-            cardIndex.value = (index + 1) % Object.keys(ServicesColors).length;
-            swipeProgress.value = 0;
-          });
-        }}
-        onSwipedAborted={() => {
-          swipeProgress.value = withTiming(0, {duration: 200});
-        }}
+    <Box style={[{ flex: 1, alignItems: "center", justifyContent: "center" }]}>
+      <BlurView
+        intensity={100}
+        style={{ width: "100%", height: "100%", alignItems: "center", justifyContent: "center" }}
+      >
+        <Carousel
+          autoPlayInterval={2000}
+          data={Services}
+          loop={true}
+          pagingEnabled={true}
+          snapEnabled={true}
+          width={window.width * 0.9}
+          height={Math.min(window.height - 64, 650)}
+          style={{
+            zIndex: 1, overflow: "visible",
+          }}
+          mode="parallax"
+          modeConfig={{
+            parallaxScrollingScale: 0.9,
+            parallaxScrollingOffset: window.width * 0.1,
+            parallaxAdjacentItemScale: 0.8
+          }}
+          onProgressChange={swipeProgress}
+          renderItem={({item}) => <ServiceCard service={item} onPress={() => {
+            if (item.id != "organizers" && item.id != "trips") {
+              router.push({
+                pathname: '/tags/[service]',
+                params: { service: item.id }
+              })
+            }
+          }}/>}
+        />
+      </BlurView>
+
+      <Animated.View
+        style={[
+          {
+            position: "absolute", zIndex: -1,
+            top: -(window.height * 0.05),
+            width: window.height * 0.1,
+            height: window.height * 0.1,
+            borderRadius: window.height * 0.1 / 2,
+            transform: [{ scaleX: window.width / (window.height * 0.1) }],
+          },
+          animatedBackgroundStyle,
+        ]}
       />
 
-      <LinearGradient
-        colors={["#FFFFFF", "transparent"]} start={{ x: 0, y: 0 }} end={{ x: 0, y: 1 }}
-        style={{
-          position: "absolute", width: "100%", height: "100%", zIndex: -1
-        }}
+      <Animated.View
+        style={[
+          {
+            position: "absolute", zIndex: -1,
+            bottom: -(window.height * 0.025),
+            width: window.height * 0.05,
+            height: window.height * 0.05,
+            borderRadius: window.height * 0.05 / 2,
+            transform: [{ scaleX: window.width / (window.height * 0.05) }],
+          },
+          animatedBackgroundStyle,
+        ]}
       />
-    </Animated.View>
+    </Box>
   )
 }
