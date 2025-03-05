8bd76585f31704c82ee8fbaaf147a29d45c3afe0
Matvey Baryshev <baryshev.matvey@yandex.ru>
2025-03-05 13:49:10 +0300

    fix: services card animation layout

--- frontend/entities/service/model/types/services.types.ts
+++ frontend/entities/service/model/types/services.types.ts
@@ -1,6 +1,6 @@
 export const Services: Service[] = [
-  { id: "events", name: "События", illustration: "events", description: "Текст, который должен быть про события" },
-  { id: "places", name: "Места", illustration: "places", description: "Текст, который должен быть про места" },
+  { id: "events", name: "События", illustration: "events", description: "Актуальные мероприятия и интересные события в Вашем городе" },
+  { id: "places", name: "Места", illustration: "places", description: "Лучшие локации для отдыха и развлечений" },
   { id: "organizers", name: "Организаторы", illustration: "organizers", description: "В разработке" },
   { id: "trips", name: "Путешествия", illustration: "trips", description: "В разработке" },
 ]
